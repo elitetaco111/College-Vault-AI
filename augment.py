c7674b43e1969c10195e974482c1c9cbad67c746
elitetaco111 <dnissly33@gmail.com>
2025-02-12 00:57:55 -0600

    Image Cleaning and Augmentation logic fixing easier pipeline from web scraping to training

--- augment.py
+++ augment.py
@@ -4,10 +4,15 @@ import os
 # Define augmentation functions using tf.image
 def augment_image(image):
     image = tf.image.random_flip_left_right(image)  # Random horizontal flip
-    image = tf.image.random_brightness(image, max_delta=0.3)  # Random brightness
-    image = tf.image.random_contrast(image, lower=0.8, upper=1.2)  # Random contrast
-    image = tf.image.random_saturation(image, lower=0.75, upper=1.25)  # Adjust saturation
+    image = tf.image.random_brightness(image, max_delta=0.2)  # Random brightness
+    image = tf.image.random_contrast(image, lower=0.9, upper=1.1)  # Random contrast
+    image = tf.image.random_saturation(image, lower=0.8, upper=1.2)  # Adjust saturation
     #image = tf.image.resize_with_crop_or_pad(image, 730, 730)  # Ensure consistent size
+    return image
+
+def clean_image(image, target_size = (511, 730)):
+    width, height = target_size
+    image = tf.image.resize_with_pad(image, height, width)
     return image
 
 def augment_and_save_images(input_dir, output_dir, num_augmentations=1):
@@ -27,18 +32,22 @@ def augment_and_save_images(input_dir, output_dir, num_augmentations=1):
                 image = tf.image.decode_jpeg(image, channels=3)  # Decode to tensor
                 #image = tf.image.resize(image, (730, 730))  # Resize
                 image = tf.cast(image, tf.uint8)  # Ensure correct type
+                cln_image = clean_image(image)
+                cln_image = tf.keras.utils.array_to_img(cln_image)
+                cln_image.save(os.path.join(save_class_path, f"cln_{img_name}"))
 
                 # Generate and save augmented images
                 for i in range(num_augmentations):
-                    aug_image = augment_image(image)
+                    aug_image = clean_image(image)
+                    aug_image = augment_image(aug_image)
                     aug_image = tf.keras.utils.array_to_img(aug_image)  # Convert tensor to PIL image
                     aug_image.save(os.path.join(save_class_path, f"aug_{i}_{img_name}"))
 
 # Define dataset paths
 input_directory = "Images"
-output_directory = "Images"
+output_directory = "aug_images"
 
 # Run augmentation and save images
-augment_and_save_images(input_directory, output_directory, num_augmentations=7)
+augment_and_save_images(input_directory, output_directory, num_augmentations=255)
 
 print("Augmented images saved successfully!")
