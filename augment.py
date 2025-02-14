cd798fd19bf9c2a8b4904771a7900ef5beac2837
DNissly <dnissly33@gmail.com>
2025-02-14 10:45:08 -0600

    improved image cleaning logic

--- augment.py
+++ augment.py
@@ -3,10 +3,11 @@ import os
 
 # Define augmentation functions using tf.image
 def augment_image(image):
-    image = tf.image.random_flip_left_right(image)  # Random horizontal flip
-    image = tf.image.random_brightness(image, max_delta=0.3)  # Random brightness
-    image = tf.image.random_contrast(image, lower=0.8, upper=1.2)  # Random contrast
-    image = tf.image.random_saturation(image, lower=0.75, upper=1.25)  # Adjust saturation
+    #image = tf.image.random_flip_left_right(image)  # Random horizontal flip
+    image = tf.image.resize_with_crop_or_pad(image, target_height = 730, target_width = 730, preserve_aspect_ratio = True)
+    #image = tf.image.random_brightness(image, max_delta=0.15)  # Random brightness
+    #image = tf.image.random_contrast(image, lower=0.9, upper=1.1)  # Random contrast
+    #image = tf.image.random_saturation(image, lower=0.9, upper=1.1)  # Adjust saturation
     #image = tf.image.resize_with_crop_or_pad(image, 730, 730)  # Ensure consistent size
     return image
 
@@ -35,10 +36,10 @@ def augment_and_save_images(input_dir, output_dir, num_augmentations=1):
                     aug_image.save(os.path.join(save_class_path, f"aug_{i}_{img_name}"))
 
 # Define dataset paths
-input_directory = "Images"
-output_directory = "Images"
+input_directory = "augmented_images"
+output_directory = "augmented_images"
 
 # Run augmentation and save images
-augment_and_save_images(input_directory, output_directory, num_augmentations=7)
+augment_and_save_images(input_directory, output_directory, num_augmentations=1)
 
 print("Augmented images saved successfully!")
